44da71af9f06623d74d9ddfc24d593bd58d152fb
gangwar <gangwarin98@gmail.com>
2017-11-03 09:46:10 +0530

    slight modifications

--- complex.cpp
+++ complex.cpp
@@ -6,10 +6,15 @@ class complex
      int real;
      int img;
 public:
+     complex()
+     {
+          real=0;
+          img=0;
+     }
      friend complex operator+(complex&, complex&);
      friend complex operator-(complex&, complex&);
      friend complex operator*(complex&, complex&);
-     friend complex operator/(complex&, complex&);
+     friend void operator/(complex&, complex&);
      friend istream& operator>>(istream&, complex&);
      friend ostream& operator<<(ostream&, complex&);
 };
@@ -21,8 +26,14 @@ int main()
      cout<<c1<<endl;
      cin>>c2;
      cout<<c2<<endl;
+     c = c1 + c2;
+     cout<<"addition of two complex numbers: "<<endl<<c<<endl;
      c = c1 - c2;
-     cout<<c;
+     cout<<"subtraction of two complex numbers: "<<endl<<c<<endl;
+     c = c1 * c2;
+     cout<<"multiplication of two complex number: "<<endl<<c<<endl;
+     cout<<"division of two complex numbers: ";
+     c1/c2;
      return 0;
 }
 
@@ -62,10 +73,17 @@ complex operator-(complex& c1, complex& c2)
 
 complex operator*(complex& c1, complex& c2)
 {
-     
+     complex ans;
+     ans.real = (c1.real * c2.real) - (c1.img * c2.img);
+     ans.img = (c1.real * c2.img) + (c1.img * c2.real);
+     return ans;
 }
 
-complex operator/(complex& c1, complex& c2)
+void operator/(complex& c1, complex& c2)
 {
-
+     int a, b, c;
+     a = (c1.real * c2.real) + (c1.img * c2.img);
+     b = (c1.img * c2.real) - (c1.real * c2.img);
+     c = (c2.real * c2.real) + (c2.img * c2.img);
+     cout<<"("<<a<<"/"<<c<<") + i("<<b<<"/"<<c<<")"<<endl;
 }
